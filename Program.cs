1067c9a318fa697df70e40dc6f3bba6ffe9a3153
tech26z <2446821304@qq.com>
2023-05-30 19:28:36 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiply_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 9; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(i * j);
+                    Console.Write(" ");
+                    if (i == j)
+                        Console.WriteLine();
+                }
+            }
+        }
+    }
+}
